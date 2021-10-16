f9e29665f84692ba3521abb2ebbea8e882113fce
Tara <tannum@gmail.com>
2021-10-17 08:55:20 +1100

    created intitial files and added products

---
+++ js/products.js
@@ -0,0 +1,31 @@
+const products = {
+    92:{
+        imgURL: "./images/clip1.jpg",
+        name: "Butterfly",
+        description: "Beautiful butterfly clip with pear decorations.",
+        price: 25,
+        quantity: 5,
+    },
+    93:{
+        imgURL: "./images/clip2.jpg",
+        name: "Flat Topaz",
+        description: "Long clip with topaz stone to brighen your hair.",
+        price: 20,
+        quantity: 5,
+    },
+    94:{
+        imgURL: "./images/clip3.jpg",
+        name: "Flower",
+        description: "Clip3",
+        price: 55,
+        quantity: 5,
+    },
+    95:{
+        imgURL: "./images/clip4.jpg",
+        name: "Pearl Stick",
+        description: "Clip4",
+        price: 45,
+        quantity: 5,
+    },
+    
+}
